e35a8d973e598a7447efebbe20dfc8c241b322aa
Kevin Jiao <kxjiao@gmail.com>
2022-04-08 01:47:52 -0400

    Update hello world API to connect to a test database

--- Code/api/app.py
+++ Code/api/app.py
@@ -1,34 +1,74 @@
 import os
 
 from flask import Flask, jsonify, request
+from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+from models import ItemsModel
 
 app = Flask(__name__)
 api = Api(app)
 
-hello_world = {}
+# Database config
+host = os.environ["POSTGRES_HOST"]
+port = os.environ["POSTGRES_PORT"]
+username = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASS"]
+database = os.environ["POSTGRES_DB"]
+
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Home(Resource):
     def get(self):
-        return "Hello World!"
+        return "Hello World! Go to /items to see a list of items, make PUT requests to /items/<key> to add or update an item."
 
 
-class HelloWorld(Resource):
-    def get(self, id):
-        # curl http://localhost:5000/{id}
-        return jsonify({"hello_world": hello_world[id]})
+class ItemList(Resource):
+    def get(self):
+        # Get all items
+        items = ItemsModel.query.all()
+        return jsonify([item.serialize for item in items])
 
-    def put(self, id):
-        # curl http://localhost:5000/{id} -d "data=Hello World" -X PUT
-        hello_world[id] = request.form["data"]
-        return jsonify({"hello_world": hello_world[id]})
+
+class Item(Resource):
+    def get(self, item_name):
+        # curl http://localhost:5000/items/{item_name}
+        try:
+            item = ItemsModel.query.filter_by(key=item_name).first_or_404()
+            return jsonify(item.serialize)
+        except:
+            return jsonify({"error": f"Item {item_name} not found"})
+
+    def put(self, item_name):
+        # curl http://localhost:5000/items/{item_name} -H 'Content-Type: application/json' -d '{"value":"Hello"}' -X PUT
+        try:
+            item = db.session.query(ItemsModel).filter_by(
+                key=item_name).first()
+            if item:
+                print(item.key, item.value, request.json["value"])
+                item.value = request.json["value"]
+                print(item.key, item.value, request.json["value"])
+                db.session.commit()
+            else:
+                item = ItemsModel(item_name, request.json["value"])
+                db.session.add(item)
+                db.session.commit()
+            return jsonify(item.serialize)
+        except:
+            return jsonify({"error": f"Error adding/updating {item_name}"})
 
 
 api.add_resource(Home, "/")
-api.add_resource(HelloWorld, "/<string:id>")
-
+api.add_resource(ItemList, "/items")
+api.add_resource(Item, "/items/<string:item_name>")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,31 +1,32 @@
 # CollegeTalk
 
-There's a lot that goes into college life for a student, but student's don't always get the information they need. CollegeTalk is an application for students to communicate with each other, giving them a space to ask questions and discuss topics freely and without judgement. This consolidates many other communication platforms that are currently being used within the student body, and provides a dedicated space for students to get their questions answered. 
+There's a lot that goes into college life for a student, but student's don't always get the information they need. CollegeTalk is an application for students to communicate with each other, giving them a space to ask questions and discuss topics freely and without judgement. This consolidates many other communication platforms that are currently being used within the student body, and provides a dedicated space for students to get their questions answered.
 
 ## Technologies Used
 
-- Frontend: Next.js (React, Typescript)
-- Backend: Python
-    - API: Flask
-- Platform/Deployment: Azure
-- Database: PostgreSQL 
+-   Frontend: Next.js (React, Typescript)
+-   Backend: Python
+    -   API: Flask
+-   Platform/Deployment: Azure
+-   Database: PostgreSQL
 
 ## Running the Code Locally
 
-- Frontend (Code/frontend):
-    1. ```cd Code/frontend```
-    2. ```npm install```
-    3. Either ```npm run start``` or ```npm run android``` or ```npm run ios```
+-   Frontend (Code/frontend):
+
+    1. `cd Code/frontend`
+    2. `npm install`
+    3. Either `npm run start` or `npm run android` or `npm run ios`
     4. Navigate to http://localhost:19002
 
-- API (Code/api):
-    1. ```cd Code/api```
-    2. Activate a venv if desired, then run ```pip install -r requirements.txt```
-    3. ```flask run```
+-   API (Code/api):
+    1. `cd Code/api`
+    2. Activate a venv if desired, then run `pip install -r requirements.txt`
+    3. `flask run`
     4. Navigate to http://localhost:5000
-    5. You can make a PUT request with ```curl http://localhost:5000/{id} -d "data=Hello World" -X PUT```, replacing {id} with a desired text. You can then make a GET request by navigating to http://localhost:5000/{id}, with {id} as the text used earlier for the PUT request.
+    5. You can make a PUT request with `curl http://localhost:5000/{id} -d "data=Hello World" -X PUT`, replacing {id} with a desired text. You can then make a GET request by navigating to http://localhost:5000/{id}, with {id} as the text used earlier for the PUT request.
 
 ## References
 
-- Frontend: https://collegetalk.azurewebsites.net/
-- API: https://collegetalk-api.azurewebsites.net/
+-   Frontend: local deployment
+-   API: https://collegetalk-api.azurewebsites.net/
