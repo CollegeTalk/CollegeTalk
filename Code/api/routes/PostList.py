0d528916fd5d6463d994024ef76d924be4621fac
Kevin Xu Jiao <kxjiao@email.wm.edu>
2022-04-18 21:17:25 -0400

    Update .gitlab-ci.yml file

--- Code/api/routes/PostList.py
+++ Code/api/routes/PostList.py
@@ -6,23 +6,18 @@ from models import PostModel, db
 
 class PostList(Resource):
     def get(self):
-<<<<<<< Code/api/routes/PostList.py
-        # Get all items
-        posts = PostModel.query.all()
-        return jsonify([post.serialize for post in posts])
-=======
         # get a limited number of posts, newest to oldest
         # curl http://localhost:5000/posts?limit=1 -H 'Content-Type: application/json'
         # get all posts, newest to oldest
         # curl http://localhost:5000/posts -H 'Content-Type: application/json'
         limit = request.args.get("limit")
         if limit != None and limit.isnumeric() and int(limit) >= 0:
-            posts = PostModel.query.order_by(PostModel.time_created.desc()).limit(int(limit))
+            posts = PostModel.query.order_by(
+                PostModel.time_created.desc()).limit(int(limit))
             return jsonify([post.serialize for post in posts])
         else:
             posts = PostModel.query.order_by(PostModel.time_created.desc())
             return jsonify([post.serialize for post in posts])
->>>>>>> Code/api/routes/PostList.py
 
     def post(self):
         # Post new item
@@ -30,7 +25,7 @@ class PostList(Resource):
         try:
             data = request.json
             post = PostModel(uuid4(), db.func.now(),
-                              data["title"], data["body"])
+                             data["title"], data["body"])
             db.session.add(post)
             db.session.commit()
             return jsonify(post.serialize)
