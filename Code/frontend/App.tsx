48aad3309a7904140fa8af11d768f6a759af8285
dtran <duketran2001@gmail.com>
2022-05-10 02:41:34 -0400

    added a splash screen

--- Code/frontend/App.tsx
+++ Code/frontend/App.tsx
@@ -2,11 +2,14 @@ import { useState, useMemo, useEffect } from "react";
 import { Alert } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AnimatedSplash from "react-native-animated-splash-screen";
+import LottieView from "lottie-react-native";
 
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import UserContext from "./UserContext";
 import { ContextUser } from "./types";
+import SchoolAnimation from "./src/assets/animations/30304-back-to-school.json";
 
 import Navigation from "./src/navigation";
 
@@ -54,16 +57,29 @@ const App = () => {
         }
     });
 
-    if (!isLoadingComplete || !initialFetched) {
+    if (!isLoadingComplete) {
         return null;
     }
     return (
-        <UserContext.Provider value={userStateContext}>
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
-                <StatusBar />
-            </SafeAreaProvider>
-        </UserContext.Provider>
+        <AnimatedSplash
+            translucent
+            isLoaded={initialFetched}
+            backgroundColor="#262626"
+            logoHeight={250}
+            logoWidth={250}
+            customComponent={
+                <LottieView source={SchoolAnimation} autoPlay loop />
+            }
+        >
+            {initialFetched ? (
+                <UserContext.Provider value={userStateContext}>
+                    <SafeAreaProvider>
+                        <Navigation colorScheme={colorScheme} />
+                        <StatusBar />
+                    </SafeAreaProvider>
+                </UserContext.Provider>
+            ) : null}
+        </AnimatedSplash>
     );
 };
 
